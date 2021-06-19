ca9cb498978819bf687682d6009b135d676b121b
Brahian Martinez <bmsistemas7@gmail.com>
2021-06-19 18:42:13 -0500

    Fix dark theme

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -33,6 +33,7 @@ final ThemeData darkThemeData = ThemeData(
   scaffoldBackgroundColor: Colors.grey.shade900,
   fontFamily: FontFamily.productSans,
   brightness: Brightness.dark,
+  primarySwatch: MaterialColor(0xFF1B5E20, darkSwatchColor),
   primaryColor: Colors.green.shade900,
   primaryColorBrightness: Brightness.dark,
   accentColor: Colors.green.shade900,
@@ -41,15 +42,29 @@ final ThemeData darkThemeData = ThemeData(
   errorColor: Colors.red.shade900,
   dividerColor: Colors.white10,
   floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: Colors.green.shade900,
-      foregroundColor: Colors.white70
+    backgroundColor: Colors.green.shade900,
+    foregroundColor: Colors.white,
   ),
   primaryTextTheme: TextTheme(
     headline6: TextStyle(
-      color: Colors.white70,
+      color: Colors.white,
     ),
   ),
   primaryIconTheme: const IconThemeData.fallback().copyWith(
-    color: Colors.white70,
+    color: Colors.white,
   ),
 );
+
+Map<int, Color> darkSwatchColor =
+{
+  50:Colors.green.shade50,
+  100:Colors.green.shade100,
+  200:Colors.green.shade200,
+  300:Colors.green.shade300,
+  400:Colors.green.shade400,
+  500:Colors.green.shade500,
+  600:Colors.green.shade600,
+  700:Colors.green.shade700,
+  800:Colors.green.shade800,
+  900:Colors.green.shade900,
+};
